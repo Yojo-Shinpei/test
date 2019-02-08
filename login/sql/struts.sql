e25f2299376a377b4b0c99cd7ac833094366ce5e
ichininosan <una.k@i.softbank.jp>
2019-02-08 21:26:55 +0900

    20190208

--- login/sql/struts.sql
+++ login/sql/struts.sql
@@ -1,6 +1,6 @@
 set names utf8;
 set foreign_key_checks = 0;
-drop datadase if exists logindb_yojo;
+drop database if exists logindb_yojo;
 create database logindb_yojo;
 use logindb_yojo;
 
@@ -8,7 +8,7 @@ create table user(
 id int,
 user_name varchar(255),
 password varchar(255)
-)
+);
 
 insert into user values
 (1,"taro","123"),
